149f8b826200539f2bc5912aedc00599fd518c24
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2022-09-10 10:59:38 -0700

    Add User signup backend endpoint

--- practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/users.js
+++ practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/users.js
@@ -1,6 +1,25 @@
 const express = require('express')
+//for signup
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+//create express router
 const router = express.Router();
 
+// Sign up
+router.post(
+  '/',
+  async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    //f the user is successfully created, then call setTokenCookie and return a JSON response with the user information
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
 
 
 
