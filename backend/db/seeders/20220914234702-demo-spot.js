ffb5b723617d2fd5962d2954ee67ff1075ea06cc
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2022-10-24 13:19:25 -0700

    fixed image url in seeder

--- backend/db/seeders/20220914234702-demo-spot.js
+++ backend/db/seeders/20220914234702-demo-spot.js
@@ -2,52 +2,57 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-  return queryInterface.bulkInsert('Spots', [
-      {
-        userId: 1,
-        address: '246 Hashed Password Way',
-        city: 'New York',
-        state: 'NY',
-        country: 'United States',
-        lat: 12.11133,
-        lng: 64.55555,
-        name: 'Secluded mansion',
-        description: 'Tech forward respite',
-        price: 299,
-        avgRating: 4.8,
-        previewImage: 'https://unsplash.com/photos/QL_DdWFUsFc'
-      },
-      {
-        userId: 2,
-        address: '55 Apple Boulevard',
-        city: 'San Francisco',
-        state: 'CA',
-        country: 'United States',
-        lat: 25.11133,
-        lng: 87.55555,
-        name: 'Low Key Home',
-        description: 'Really nice and comfy home',
-        price: 399,
-        avgRating: 4.9,
-        previewImage: 'https://images.unsplash.com/photo-1601056645918-329b756e54fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2934&q=80'
-      },
-      {
-        userId: 3,
-        address: '888 CodingAlong Dr',
-        city: 'Chattanooga',
-        state: 'TN',
-        country: 'United States',
-        lat: 25.33333,
-        lng: 64.55555,
-        name: 'Country Getaway',
-        description: 'Cute chill country home',
-        price: 199,
-        avgRating: 4.9,
-        previewImage: 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-      },
-
-
-    ], {});
+  return queryInterface.bulkInsert(
+		"Spots",
+		[
+			{
+				userId: 1,
+				address: "246 Hashed Password Way",
+				city: "New York",
+				state: "NY",
+				country: "United States",
+				lat: 12.11133,
+				lng: 64.55555,
+				name: "Secluded mansion",
+				description: "Tech forward respite",
+				price: 299,
+				avgRating: 4.8,
+				previewImage:
+					"https://images.unsplash.com/photo-1618990908950-fd1a23294d11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+			},
+			{
+				userId: 2,
+				address: "55 Apple Boulevard",
+				city: "San Francisco",
+				state: "CA",
+				country: "United States",
+				lat: 25.11133,
+				lng: 87.55555,
+				name: "Low Key Home",
+				description: "Really nice and comfy home",
+				price: 399,
+				avgRating: 4.9,
+				previewImage:
+					"https://images.unsplash.com/photo-1601056645918-329b756e54fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2934&q=80",
+			},
+			{
+				userId: 3,
+				address: "888 CodingAlong Dr",
+				city: "Chattanooga",
+				state: "TN",
+				country: "United States",
+				lat: 25.33333,
+				lng: 64.55555,
+				name: "Country Getaway",
+				description: "Cute chill country home",
+				price: 199,
+				avgRating: 4.9,
+				previewImage:
+					"https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+			},
+		],
+		{}
+	);
   },
 
   async down (queryInterface, Sequelize) {
--- backend/db/seeders/20220914234801-demo-image.js
+++ backend/db/seeders/20220914234801-demo-image.js
@@ -3,30 +3,32 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-  return queryInterface.bulkInsert('Images', [
-     {
-        spotId:  1,
-        reviewId: 1,
-        url: 'https://unsplash.com/photos/QL_DdWFUsFc',
-        preview: true,
+  return queryInterface.bulkInsert(
+		"Images",
+		[
+			{
+				spotId: 1,
+				reviewId: 1,
+				url: "https://images.unsplash.com/photo-1618990908950-fd1a23294d11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+				preview: true,
+			},
 
-      },
+			{
+				spotId: 2,
+				reviewId: 2,
+				url: "https://images.unsplash.com/photo-1601056645918-329b756e54fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2934&q=80",
+				preview: true,
+			},
 
-     {
-        spotId:  2,
-        reviewId: 2,
-        url: 'https://images.unsplash.com/photo-1601056645918-329b756e54fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2934&q=80',
-        preview: true,
-     },
-
-      {
-        spotId:  3,
-        reviewId: 3,
-        url: 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        preview: true,
-     },
-
-    ], {});
+			{
+				spotId: 3,
+				reviewId: 3,
+				url: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+				preview: true,
+			},
+		],
+		{}
+	);
   },
 
   async down (queryInterface, Sequelize) {
